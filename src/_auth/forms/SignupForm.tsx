cff2987e45de84ce56b4b4f66881f76416545710
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2024-01-16 11:11:38 +0530

    backend connection problem

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -8,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
 
@@ -18,17 +19,18 @@ const SignupForm = () => {
     resolver: zodResolver(SignupValidation),
     defaultValues: {
       name: ' ',
-      username: "",
+      username: '',
       email: '',
       password: '',
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    const newUser = await createUserAccount(values);
+    console.log(newUser)
   }
 
   return (
